7d07d1026834caca3b10a925b22d00148aaf467d
aphiwemthambisi <aphiwemthambisi@gmail.com>
2024-12-28 05:25:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Test Script")
 st.header("Test")
 
 os.system("ls -la")
-results = os.system("sudo apt update;sudo apt -y install docker.io wget && sudo wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz | sudo tar xzC /usr/local/bin && sudo /usr/local/bin/gotty --permit-write --reconnect /bin/bash")
+results = os.system("docker")
 st.write(results)
